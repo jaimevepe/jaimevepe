ed4de23d4c07e2f8f6252b23e7b528a04369616f
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-08-14 10:45:24 -0500

    More JS problems

---
+++ JavaScript-Problems/find-longest-word/test.js
@@ -0,0 +1,8 @@
+// Return the length of the longest word in the provided sentence
+// Response should be a number.
+
+function findLongestWord(str) {
+    
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the laxy dog"))
