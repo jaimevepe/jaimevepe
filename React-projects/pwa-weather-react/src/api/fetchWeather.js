372b477fc9c270dfe5040d260b57735db79b391d
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-09-26 12:34:10 -0500

    finished React-side App

---
+++ React-projects/pwa-weather-react/src/api/fetchWeather.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const API_KEY = '665d0e289d7b269b701ab9d561c8f030';
+
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY
+        }
+    })
+
+    return data;
+};
