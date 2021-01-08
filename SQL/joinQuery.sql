41f56e4ea300305a12c8fd91e18a6093bef22b26
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-01-07 22:03:50 -0600

    finished Join lesson 1-07-21

--- SQL/joinQuery.sql
+++ SQL/joinQuery.sql
@@ -22,5 +22,52 @@ JOIN hr.locations l ON d.location_id = l.location_id
 -- locations and department have a location_id
 
 ------------------------
---    INNER JOIN      --
+--     LEFT JOIN      --
+SELECT department_id, department_name
+FROM hr.departments;
 
+SELECT 
+    first_name,
+    e.department_id,
+    department_name
+FROM hr.employees e LEFT JOIN hr.departments d
+ON e.department_id = d.department_id;
+
+
+-- RIGHT JOIN
+
+SELECT
+    first_name,
+    department_name
+FROM hr.employees e RIGHT JOIN hr.departments d
+ON e.department_id = d.department_id;
+
+--Need sales date, product name, sales amount
+SELECT 
+s.saledate AS "Date Sold",
+p.description AS "Item Sold",
+s.amount AS "Sale Amount"
+FROM hr.sales s JOIN hr.products p
+ON p.productid = s.productid;
+
+-- To See saledate as a Regular date 
+SELECT to_char(
+s.saledate, 'MM/DD/YYYY') AS "Date Sold",
+p.description AS "Item Sold",
+s.amount AS "Sale Amount"
+FROM hr.sales s JOIN hr.products p
+ON p.productid = s.productid;
+
+--provide first, last, height, weight, team
+-- Alter above to include players who have not been drafted
+SELECT *
+FROM hr.players;
+
+SELECT 
+p.fname, 
+p.lname, 
+p.height,
+p.weight,
+t.name
+FROM hr.players p LEFT JOIN hr.teams t
+ON p.team_id = t.id;
