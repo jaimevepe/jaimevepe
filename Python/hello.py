1847dda4867a6927d4cdf62ff62f70a6eacc66f9
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-01-18 14:32:01 -0600

    learning in progress

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,20 @@
 print('Hello World!')
+# print is the same as console.log in JS
+x = 6
+print(x)
+
+x = x + 1
+print(x)
+
+# While Loop
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1
+print('Blastoff!')
+
+# Variables
+# TODO: Constants - Fixed values such as numbers, letter, and strings,are called "constants"
+# EX: x = 12.2, X is the variable and 12.2 the value
+
+
