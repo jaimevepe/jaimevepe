45a3c696ae082318f4c334f948bd6f6c7f524388
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-01-20 15:05:14 -0600

    test commit

--- Python/Exercises/ex-02/ex2.py
+++ Python/Exercises/ex-02/ex2.py
@@ -2,4 +2,4 @@ hours = input("Enter Hours: ")
 rate = input("Eenter Rate: ")
 # hours are type string inputs, so we have to convert them into Numbers
 xp = float(hours) * float(rate)
-print("Pay:",xp)
+print("Your Pay:",xp)
