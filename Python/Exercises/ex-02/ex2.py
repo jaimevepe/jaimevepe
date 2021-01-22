c70ee67bc97ae8e5aadc99f4fa31602ea0a0b956
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-01-22 13:35:47 -0600

    finished part 2 of Exercise ex-02

--- Python/Exercises/ex-02/ex2.py
+++ Python/Exercises/ex-02/ex2.py
@@ -1,5 +1,26 @@
+# Write computation to pay workers from hours worked and oay rate
+
 hours = input("Enter Hours: ")
-rate = input("Eenter Rate: ")
+rate = input("Enter Rate: ")
 # hours are type string inputs, so we have to convert them into Numbers
 xp = float(hours) * float(rate)
 print("Your Pay:",xp)
+
+#----------------------------------------------------------------#
+# Rewrite pay computation to pay workers time and a half(0.5) for working more than 40 hours
+
+hours2 = input("Enter hours again: ")
+rate2 = input("Enter rate: ")
+fhour = float(hours2) # convert to decimal numbers
+fRate = float(rate2)
+
+# print(fhour, fRate)
+if fhour > 40 :
+    print('OverTime')
+    regPay = fhour * fRate
+    overTimePay = (fhour - 40.0) * (fRate * 0.5)
+    pay = regPay + overTimePay
+else:
+    print('Regular Pay')
+    pay = fhour * fRate
+print("PAY with TRY/EXCEPT",pay)
---
+++ Python/test.py
@@ -0,0 +1,43 @@
+# Try / Except Structure
+
+# You surround a dangerous section of code with TRY and EXCEPT
+# If the code in the TRY works - the EXCEPT is Skipped
+# If the code in the TRY fails - it jumps to the EXCEPT section
+
+greet = 'Hello Bob!'
+try: 
+    isGreet = int(greet) #turned it into a number (dangerous code)
+except:
+    isGreet = -1
+print("First", isGreet)
+
+greet = '123'
+try:
+    isGreet = int(greet) # dangerous code
+except:
+    isGreet = -1
+print("Second", isGreet)
+
+# Entering Data
+
+rawstr = input('Enter a number:')
+try:
+    convertToNum = int(rawstr) # converts your input into a number,
+                               # as long as you entered a number and not string
+except:
+    convertToNum = -1
+
+if convertToNum > 0:
+    print("Nice work",convertToNum)
+else:
+    print("Not a number")
+
+####################################################
+temp = '5' # if temp had a word string insde, it would skip to except
+cel = 0
+try:
+    fahr = float(temp) # turn it into a number with decimal
+    cel = (fahr - 32.0) * 5.0 / 9.0
+except:
+    print('Something went wrong!')
+print('Done, this is the temp', cel)
