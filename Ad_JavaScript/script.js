36243b7397f016238e52e428cf2760aa2104f044
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-01-21 21:28:07 -0600

    understanding AD JS concepts

---
+++ Ad_JavaScript/script.js
@@ -0,0 +1,29 @@
+// function outer(){
+//     console.log("Outer!")
+//     inner()
+// }
+
+// function inner(){
+//     console.log("Inner!")
+// }
+
+// outer()
+
+// console.log(myVar)
+// var myVar = "Value of myVar"
+
+// function myFunc(){
+//     console.log("Output from function myFunc");
+// };
+
+// console.log(myVar)
+// myFunc()
+
+var a = 10;
+
+if(a === 10){
+    let a = 40;
+    console.log("Inside a: ", a)
+}
+console.log(`Outisde a: ${a}`);
+
