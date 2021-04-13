72a1acb1678cedab6d11526b54ec31a68c70774d
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-04-13 15:25:30 -0500

    Python Modules practice

---
+++ Python/Modules/circle_ractangle_area/rectangle.py
@@ -0,0 +1,12 @@
+# The rectangle module has functions that perform
+# calculations related to rectangles.
+
+# The area function accepts a rectangle's width and
+# length as arguments and returns the rectangle's area.
+def area(width, length):
+    return width * length
+
+# The area function accepts a rectangle's width and
+# length as arguments and returns the rectangle's area.
+def perimeter( width, length):
+    return 2 * (width + length)
