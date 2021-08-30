4aa2dff103faa3dc8416b2f832c0534c6041a0c4
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-08-29 21:52:47 -0500

    updatting

--- Rest-API/Rest-API-practice/server.js
+++ Rest-API/Rest-API-practice/server.js
@@ -6,7 +6,7 @@ import usersRoutes from './routes/users.js';
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(bodyParser.json()); // that we will use json data for the app
+app.use(express.json()); // that we will convert data into njson data for the app
 
 app.use('/users', usersRoutes);
 
